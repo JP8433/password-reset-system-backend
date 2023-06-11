a77ed49eeefe03183c87605923a9eb7b7ed9ee2b
Jay Patel <68432630+JP8433@users.noreply.github.com>
2023-06-11 16:27:20 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+export default{
+    JWT_SECRET:"rS6PNgS5GZM6zrOntjx99Xg2/56ZCuKAqS+3i1UoELA=",
+    EMAIL:"idea8433@gmail.com",
+    PASSWORD:"jvzhjsbutfxdmqjg",
+    ATLAS_URI:"mongodb+srv://jaypatelin9167:Jay8433$@cluster0.zypdpxx.mongodb.net/"
+}
