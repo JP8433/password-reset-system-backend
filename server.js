80800cefd3b933b3360d34f214e33e5945911f61
Jay Patel <68432630+JP8433@users.noreply.github.com>
2023-06-11 19:11:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,29 +8,36 @@ const app = express();
 
 /** middlewares */
 app.use(express.json());
-app.use(cors({
-  origin: 'https://passwordresetflowbyjp.netlify.app/', // Replace with your frontend URL
-}));
+app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // less hackers know about our stack
+
 
 const port = 8080;
 
 /** HTTP GET Request */
 app.get('/', (req, res) => {
-  res.status(201).json("Home GET Request");
+    res.status(201).json("Home GET Request");
 });
 
-/** api routes */
-app.use('/api', router);
 
-/** start server only when we have a valid connection */
-connect()
-  .then(() => {
-    app.listen(port, () => {
-      console.log(`Server connected to http://localhost:${port}`);
-    });
-  })
-  .catch(error => {
+/** api routes */
+app.use('/api', router)
+
+/** start server only when we have valid connection */
+connect().then(() => {
+    try {
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        })
+    } catch (error) {
+        console.log('Cannot connect to the server')
+    }
+}).catch(error => {
     console.log("Invalid database connection...!");
-  });
+})
+
+
+   
+  
+  
